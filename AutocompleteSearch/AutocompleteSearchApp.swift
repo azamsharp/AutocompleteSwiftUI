2dca0aa4786da1a0cfba751e25816af332a55ec7
Mohammad Azam <azamsharp@gmail.com>
2021-12-13 08:06:58 -0600

    Initial Commit

---
+++ AutocompleteSearch/AutocompleteSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutocompleteSearchApp.swift
+//  AutocompleteSearch
+//
+//  Created by Mohammad Azam on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct AutocompleteSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
